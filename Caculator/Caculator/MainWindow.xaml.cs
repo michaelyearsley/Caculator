fa8e150fe9265a2a45b370eaeed89bdfde1317e7
Michael Yearsley <59950440+michaelyearsley@users.noreply.github.com>
2020-03-02 19:58:40 -0800

    Add files via upload

--- Caculator/Caculator/MainWindow.xaml.cs
+++ Caculator/Caculator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Caculator
 {
@@ -20,111 +9,144 @@ namespace Caculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        float number1 = 0;
-        float number2 = 0;
+        String number1 = string.Empty;
+        String number2 = string.Empty;
         String operation = string.Empty;
         Button b;
-        String Num = string.Empty;
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             b = (Button)sender;
             if (operation == "")
             {
-                number1 = (number1 * 10) + Convert.ToInt32(b.Content);
-                Results.Text = number1.ToString();
+                if (Convert.ToString(b.Content) == ".")
+                {
+                    if (!Results.Text.Contains("."))
+                    {
+                        number1 = number1 + Convert.ToString(b.Content);
+                        Results.Text = number1.ToString();
+                    }
+                }
+                else
+                {
+                    number1 = number1 + Convert.ToString(b.Content);
+                    Results.Text = number1.ToString();
+                }
             }
             else
             {
-                number2 = (number2 * 10) + Convert.ToInt32(b.Content);
-                Results.Text = number2.ToString();
+                if (Convert.ToString(b.Content) == ".")
+                {
+                    if (!Results.Text.Contains("."))
+                    {
+                        number2 = number2 + Convert.ToString(b.Content);
+                        Results.Text = number2.ToString();
+                    }
+                }
+                else
+                {
+                    number2 = number2 + Convert.ToString(b.Content);
+                    Results.Text = number2.ToString();
+                }
             }
         }
         private void Operation_Click(object sender, RoutedEventArgs e)
         {
-            b = (Button)sender;
-            operation = Convert.ToString(b.Content);
-            Results.Text = "0";
+            if (operation == "=" || operation == "")
+            {
+                b = (Button)sender;
+                operation = Convert.ToString(b.Content);
+                Results.Text = "0";
+            }
         }
         private void Equal_Click(object sender, RoutedEventArgs e)
         {
-            switch(operation)
+            b = (Button)sender;
+            if (operation != "")
             {
-                case "+":
-                    number1 = (number1 + number2);
-                    number2 = 0;
-                    Results.Text = number1.ToString();
-                    break;
-                case "-":
-                    number1 = (number1 - number2);
-                    number2 = 0;
-                    Results.Text = number1.ToString();
-                    break;
-                case "*":
-                    number1 = (number1 * number2);
-                    number2 = 0;
-                    Results.Text = number1.ToString();
-                    break;
-                case "/":
-                    number1 = (number1 / number2);
-                    number2 = 0;
-                    Results.Text = number1.ToString();
-                    break;
+                switch (operation)
+                {
+                    case "+":
+                        number1 = Convert.ToString(Math.Round((double.Parse(number1) + double.Parse(number2)), 2));
+                        number2 = string.Empty;
+                        operation = Convert.ToString(b.Content);
+                        Results.Text = number1;
+                        break;
+                    case "-":
+                        number1 = Convert.ToString(Math.Round((double.Parse(number1) - double.Parse(number2)), 2));
+                        number2 = string.Empty;
+                        operation = Convert.ToString(b.Content);
+                        Results.Text = number1;
+                        break;
+                    case "*":
+                        number1 = Convert.ToString(Math.Round((double.Parse(number1) * double.Parse(number2)), 2));
+                        number2 = string.Empty;
+                        operation = Convert.ToString(b.Content);
+                        Results.Text = number1;
+                        break;
+                    case "/":
+                        number1 = Convert.ToString(Math.Round((double.Parse(number1) / double.Parse(number2)), 2));
+                        number2 = string.Empty;
+                        operation = Convert.ToString(b.Content);
+                        Results.Text = number1;
+                        break;
+                }
             }
         }
-
         private void CE_Click(object sender, RoutedEventArgs e)
         {
             if (operation == "=")
             {
-                number2 = 0;
+                number2 = string.Empty;
                 Results.Text = "0";
             }
             else if (operation == "")
             {
-                number1 = 0;
+                number1 = string.Empty;
                 Results.Text = "0";
             }
             else
             {
-                number2 = 0;
+                number2 = string.Empty;
                 Results.Text = "0";
             }
         }
         private void C_Click(object sender, RoutedEventArgs e)
         {
-            number1 = 0;
-            number2 = 0;
+            number1 = string.Empty;
+            number2 = string.Empty;
             operation = "";
             Results.Text = "0";
         }
 
         private void BackSpace_Click(object sender, RoutedEventArgs e)
         {
-            if (operation == "")
+            if (operation == "=")
             {
-                Num = Convert.ToString(number1);
-                number1 = int.Parse(Num.Remove(Num.Length - 1, 1));
                 Results.Text = number1.ToString();
             }
+            else if(operation == "")
+            {
+                number1 = number1.Remove(number1.Length - 1, 1);
+                Results.Text = number1.ToString();
+            }
+            
             else
             {
-                Num = Convert.ToString(number2);
-                number2 = int.Parse(Num.Remove(Num.Length - 1, 1));
+                number2 = number2 = number1.Remove(number2.Length - 1, 1);
                 Results.Text = number2.ToString(); ;
             }
 
         }
-
         private void PosNeg_Click(object sender, RoutedEventArgs e)
         {
             if (operation == "")
             {
-                number1 *= -1;
+                number1=Convert.ToString(double.Parse(number1) * -1);
                 Results.Text = number1.ToString();
             }
             else
             {
-                number2 *= -1;
+                number2 = Convert.ToString(double.Parse(number1) * -1);
                 Results.Text = number2.ToString();
             }
         }
